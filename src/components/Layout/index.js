0c916307e37d938a679c7c295d3c296619809885
Ouail-01 <ouaillaarabi@gmail.com>
2023-10-05 03:29:28 +0100

    Add the sideBar

---
+++ src/components/Layout/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import Sidebar from '../Sidebar';
+import './index.scss';
+
+const Layout = () => (
+  <div className="App">
+    <Sidebar />
+  </div>
+);
+
+export default Layout;
