c46fa47d5425f3bc46f098c6e16c122006c16546
Ouail-01 <ouaillaarabi@gmail.com>
2023-10-12 05:16:59 +0100

    Add new project

--- src/components/Work/projectData.js
+++ src/components/Work/projectData.js
@@ -48,12 +48,12 @@ const projects = [
     projLink: "https://space-travelers-hub-rw29.onrender.com/rockets",
   },
   {
-    title: "Keeping track of hundreds of components",
+    title: "Bookstore_Project",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+      "A web application where users can add and remove books. The homepage displays a list of all added books. Data is stored and fetched from API endpoints.",
     imgUrl: projImg6,
-    githubLink: "https://github.com/Ouail-01",
-    projLink: "https://github.com/Ouail-01",
+    githubLink: "https://github.com/Ouail-01/Bookstore_Project",
+    projLink: "https://ouail-01.github.io/Bookstore_Project/",
   },
 ];
 
